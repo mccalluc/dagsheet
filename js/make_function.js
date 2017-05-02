9bbdff6dcaad42cdfcbccdf9f0681726f61d5736
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-05-02 14:06:58 -0400

    Checkpoint

--- js/make_function.js
+++ js/make_function.js
@@ -1,10 +1,17 @@
 // TODO: Do we trust the code we're running or not?
 
 function make_function(parameter_names, body) {
-  var for_apply = parameter_names.slice();
-  for_apply.push(body);
-  return Function.apply({}, for_apply)
+  var for_apply = parameter_names.slice(); // shallow copy
+  var statements = body.split(';'); // TODO: actually parse the JS
+  statements[statements.length-1] =
+      'return ' + statements[statements.length-1];
+  var mod_body = statements.join(';');
+  for_apply.push(mod_body);
+  return Function.apply(
+      {}, // "this"
+      for_apply
+  )
 }
 
-var f = make_function(['a', 'b'], 'return a + b');
-console.log('2+2', f(2, 2));
+var f = make_function(['greeting', 'name'], 'console.log("this", this); greeting + ", " + name + "!"');
+console.log(f({greeting: 'hello', name: 'hello'}));
