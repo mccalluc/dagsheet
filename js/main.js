cb8120b6746ccb8a1869d036136cf150213d8ce0
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-05-02 09:30:09 -0400

    Cell value edit works

--- js/main.js
+++ js/main.js
@@ -1,18 +1,55 @@
 function main(container) {
-    if (!mxClient.isBrowserSupported()) {
-        mxUtils.error('Browser is not supported!', 200, false);
-    } else {
-        mxEvent.disableContextMenu(container);
-        var graph = new mxGraph(container);
-        new mxRubberband(graph);
-        var parent = graph.getDefaultParent();
-        graph.getModel().beginUpdate();
-        try {
-            var v1 = graph.insertVertex(parent, null, 'Hello,', 20, 20, 80, 30);
-            var v2 = graph.insertVertex(parent, null, 'World!', 200, 150, 80, 30);
-            var e1 = graph.insertEdge(parent, null, '', v1, v2);
-        } finally {
-            graph.getModel().endUpdate();
-        }
+  if (!mxClient.isBrowserSupported()) {
+    mxUtils.error('Browser not supported', 200, false);
+  } else {
+    mxEvent.disableContextMenu(container);
+
+    var graph = new mxGraph(container);
+    graph.setHtmlLabels(true);
+    graph.setAllowDanglingEdges(false);
+    graph.setDisconnectOnMove(false);
+
+    new mxKeyHandler(graph);
+    new mxRubberband(graph);
+
+    graph.getLabel = function(cell) {
+      var div = document.createElement('div');
+      var label;
+      if (cell.isVertex()) {
+        label = cell.value.formula;
+      } else if (cell.isEdge()) {
+        label = cell.value.label;
+      }
+      mxUtils.write(div, label);
+      return div;
+    };
+
+    graph.getEditingValue = function(cell, event) {
+      if (cell.isVertex()) {
+        return cell.value.formula;
+      } else if (cell.isEdge()) {
+        return cell.value.label;
+      }
+    };
+
+    graph.getModel().valueForCellChanged = function(cell, value) {
+      var previous = cell.value;
+      if (cell.isVertex()) {
+        cell.value.formula = value;
+      } else if (cell.isEdge()) {
+        cell.value.label = value;
+      }
+      return previous;
+    };
+
+    var parent = graph.getDefaultParent();
+    graph.getModel().beginUpdate();
+    try {
+      var v1 = graph.insertVertex(parent, null, {formula: 'Hello,'}, 20, 20, 80, 30);
+      var v2 = graph.insertVertex(parent, null, {formula: 'World!'}, 200, 150, 80, 30);
+      var e1 = graph.insertEdge(parent, null, {label: 'parent'}, v1, v2);
+    } finally {
+      graph.getModel().endUpdate();
     }
+  }
 };
